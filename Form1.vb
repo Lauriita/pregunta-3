b041e7ea1b7c22611fc67abc36dc1f888bd81ff3
Lauriita <laurahb123@gmail.com>
2020-02-03 20:52:12 +0100

    Agregar archivos de proyecto.

---
+++ Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+
+    Dim a(5, 5) As Integer
+    Dim random As New Random
+    Dim i, j As Integer
+    Dim cadena As String
+    Dim suma As Integer
+    Dim total As Integer
+    Dim mayor As Integer = 0
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Randomize()
+
+        For i = 0 To 4
+            For j = 0 To 4
+                a(i, j) = random.Next(0, 9)
+                cadena = cadena & a(i, j)
+                suma = suma + a(i, j)
+            Next
+            cadena = cadena & " " & suma & vbNewLine
+            If mayor < total Then
+
+                mayor = total
+
+            ElseIf mayor > total Then
+
+                mayor = mayor
+            End If
+            total = suma
+            suma = 0
+        Next
+
+        cadena = cadena & vbNewLine & "El mayor es = " & mayor
+
+
+        TextBox1.Text = cadena
+
+    End Sub
+End Class
